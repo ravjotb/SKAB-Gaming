e57e6628ab701282bd3c86de4c5eb505e88fa1bf
Saad <sabdulra@sfu.ca>
2020-11-07 05:21:25 -0800

    Added main and question pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "SKAB-Gaming" });
 });
 
+router.get("/play", function (req, res, next) {
+  res.render("question", {
+    title: "SKAB-Gaming",
+    question: "What is the capital of Canada?",
+    imgURL:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Canada_flag_map.svg/1000px-Canada_flag_map.svg.png",
+    optionA: "Vancouver",
+    optionB: "Toronto",
+    optionC: "Ottawa",
+    optionD: "Calgary",
+  });
+});
 module.exports = router;
