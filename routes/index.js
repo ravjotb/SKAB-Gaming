ea21c01549113d51146b190647330ca26c5807cd
jasdeepgill <jasdeep360@gmail.com>
2020-12-05 21:03:41 -0800

    added profile index and changed homepage index

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,23 @@ const passport= require('passport');
 const {isLoggedIn}= require('../middleware/index')
 /* GET home page. */
 
-router.get("/", async function (req, res, next) {
-/*  try{
-    await Question.deleteMany({})
+// router.get("/", async function (req, res, next) {
+//   try{
+//     await Question.deleteMany({})
+//   }
+//   catch(err){
+//     console.log(err);
+//   }
+//   res.render("index", { title: "SKAB-Gaming" });
+// });
+router.get('/', async function(req, res, next){
+  try {
+    var allusers= await User.find().sort({'wins':-1}).exec();
+    res.render('index', {allusers});
+
+  } catch (e) {
+    console.log(e);
   }
-  catch(err){
-    console.log(err);
-  }*/
-  res.render("index", { title: "SKAB-Gaming" });
 });
 
 router.get("/register", function(req, res, next){
@@ -120,6 +129,16 @@ router.get('/join-game', isLoggedIn, async function(req, res, next){
   }
 });
 
+router.get('/profile', isLoggedIn, async function(req, res, next){
+  try {
+    var allusers= await User.find().sort({'wins':-1}).exec();
+    res.render('profile', {allusers});
+
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 router.get('/room/:id', isLoggedIn, async function(req, res, next){
   try{
     var game=await Game.findById(req.params.id).populate('creator').populate('activePlayers').populate('questions').populate('winner').exec();
@@ -160,6 +179,3 @@ module.exports = router;
 
 
 
-router.get("/profile", function (req, res, next) {
-  res.render("profile")
-})
